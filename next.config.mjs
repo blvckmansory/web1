f3393f3da6f17827f0f60d4345ed0877f139e9b6
zkharov <zakharovvadzim@yandex.by>
2024-02-14 16:10:22 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-const NODE_ENV = process.env.NODE_ENV
-const STRAPI_URL = process.env.STRAPI_URL
-const STRAPI_PORT = process.env.STRAPI_PORT
 const STRAPI_DOMAIN = process.env.STRAPI_DOMAIN
 
 /** @type {import('next').NextConfig} */
@@ -18,30 +15,7 @@ const config = {
 			destination: '/faq/hints',
 		},
 	],
-	async headers() {
-		return [
-			{
-				source: '/api/(.*)',
 
-				headers: [
-					{
-						key: 'Access-Control-Allow-Origin',
-						value: STRAPI_URL,
-					},
-
-					{
-						key: 'Access-Control-Allow-Methods',
-						value: 'GET, POST, PUT, DELETE, OPTIONS',
-					},
-
-					{
-						key: 'Access-Control-Allow-Headers',
-						value: 'Content-Type',
-					},
-				],
-			},
-		]
-	},
 	images: {
 		minimumCacheTTL: 31536000,
 		dangerouslyAllowSVG: true,
