c769665a602f275e7ac0a7514de41bff6ff7b496
artemushkaaa <shamaich007@gmail.com>
2024-09-12 14:59:15 +0300

    test

--- src/shared/config/feature-card.tsx
+++ src/shared/config/feature-card.tsx
@@ -60,7 +60,7 @@ const FEATURE_BUSINESS_CARD_CONFIG = [
 	{
 		icon: 'scroll-text',
 		title: 'Предоставление отчёта',
-		description: 'Ежемесячный отчёт предоставляется до **14 числа.**',
+		description: 'Ежемесячный отчёт предоставляется до **16 числа.**',
 	},
 	{
 		icon: 'blocks',
